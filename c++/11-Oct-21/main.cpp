a7c48b9389fd003d856f9a61cf064cef7c0771e0
Reiku <Kisarag1@gmail.com>
2021-11-06 12:34:30 +0300

    CPP

--- c++/11-Oct-21/main.cpp
+++ c++/11-Oct-21/main.cpp
@@ -1,28 +1,42 @@
 #include <iostream>
-#include <thread>
+#include <math.h>
 
-#define THREADS 8
-
-int c = 0;
-void check(int from, int to){
-    for (int i = from; i < to; ++i) {
+int first(){
+    int c = 0;
+    for (int i = 0; i < 1000000; ++i) {
         for (int j = 0; j < 1000000; ++j) {
             if (i*i+j*j<1000000) c++;
             else break;
         }
     }
+    return c;
+}
+
+void second(int n){
+    for (int i = 0; i <= n/2; ++i) {
+        std::cout << n-(i*2) << "\t" << i << std::endl;
+    }
+}
+
+
+double SSin(double x, int n){
+    for (int i = 0; i < n; ++i) {
+        x = sin(x);
+    }
+    return x;
+}
+
+double third(double x, int n){
+    double r=0;
+    for (int i = 0; i < n; ++i) {
+        r+= SSin(x, i);
+    }
+    return r;
 }
 
 int main(){
-    auto* array = new std::thread[THREADS];
-    for (int i = 0; i < THREADS; ++i) {
-        array[i] = std::thread(check, i*1000000/THREADS, (i+1)*1000000/THREADS);
-    }
-
-    for (int i = 0; i < THREADS; ++i) {
-        array[i].join();
-    }
-
-    std::cout << c;
+    std::cout << first() << std::endl << "________" << std::endl;
+    second(11);
+    std::cout << "________" << std::endl << third(1000, 10) << std::endl;
     return 0;
 }
